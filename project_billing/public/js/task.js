32a13a51cc93f4553149763a5ddd1f11e534b9c3
Anoop Kurungadam <anoop@earthianslive.com>
2021-05-05 12:23:44 +0530

    fix: custom fields ordering, task item fields readonly

--- project_billing/public/js/task.js
+++ project_billing/public/js/task.js
@@ -4,8 +4,7 @@ frappe.ui.form.on('Task', {
             frm.set_df_property('billable_amount', 'read_only', 1);
             frm.set_df_property('is_milestone', 'read_only', 1);
         }
-        if (frm.doc.item) {
-            frm.set_df_property('task_group', 'read_only', 1);
+        if (frm.doc.task_item) {
             frm.set_df_property('task_item', 'read_only', 1);
             frm.set_df_property('task_item_uom', 'read_only', 1);
             frm.set_df_property('task_item_group', 'read_only', 1);
